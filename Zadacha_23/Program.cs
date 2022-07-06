10ad21d079cfc2808fcd85e174ac96172b57fbdb
Starostina <pankovays@mail.ru>
2022-07-06 12:58:56 +0700

    Сделала задачу 23 к семинару 3

---
+++ Zadacha_23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+System.Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    System.Console.WriteLine($"Куб числа {i} равен {Math.Pow(i, 3)}");
+    //System.Console.WriteLine($"Куб числа {i} равен {i*i*i}");
+}
