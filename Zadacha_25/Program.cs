f13d4811e8ce6bd1dd385220763323696f953e6d
Starostina <pankovays@mail.ru>
2022-07-07 14:17:35 +0700

    Сделала задачу 25 к ДЗ семинар 4

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+System.Console.WriteLine("Введите числа через запятую: ");
+string[] numbers = Console.ReadLine()!.Split(',');
+int num = int.Parse(numbers[0]);
+int grade = int.Parse(numbers[1]);
+if (grade > 0)
+{
+    Console.WriteLine($"Возведение числа {num} в натуральную степень {grade} равна {Math.Pow(num, grade)}");
+}
+else
+{
+    System.Console.WriteLine($"Error! Натуральная степень должна быть > 0 ");
+}
+
